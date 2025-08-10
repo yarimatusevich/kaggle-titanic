9d0ca966bfebc71c0659a72b5cb92a02b3e0223a
Yaroslav Matusevich <yari.matusevich@gmail.com>
2025-08-10 19:21:25 -0400

    Updated gitignore to ignore data folders

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 import pandas as pd
+import gzip
 from pandas import DataFrame
-
 
 def create_data_frame(data_file_name: str):
     return pd.read_csv(data_file_name, sep='\t')
@@ -12,25 +12,21 @@ def parse_data(data: DataFrame):
 df = create_data_frame('prediction.csv')
 print(df.to_string())
 
-
-import pandas as pd
-import gzip
+rna_data = pd.read_csv('data/Hackathon2024.RNA.txt', sep='\t', index_col=0)
 
 
-rna_data = pd.read_csv('Hackathon2024.RNA.txt.gz', sep='\t', index_col=0)
+atac_data = pd.read_csv('data/Hackathon2024.ATAC.txt.gz', sep='\t', index_col=0)
 
 
-atac_data = pd.read_csv('Hackathon2024.ATAC.txt.gz', sep='\t', index_col=0)
+train_pairs = pd.read_csv('data/Hackathon2024.Training.Set.Peak2Gene.Pairs.txt.gz', sep='\t')
 
 
-train_pairs = pd.read_csv('Hackathon2024.Training.Set.Peak2Gene.Pairs.txt.gz', sep='\t')
+test_pairs = pd.read_csv('data/Hackathon2024.Testing.Set.Peak2Gene.Pairs.txt.gz', sep='\t')
 
 
-test_pairs = pd.read_csv('Hackathon2024.Testing.Set.Peak2Gene.Pairs.txt.gz', sep='\t')
+meta_data = pd.read_csv('data/Hackathon2024.Meta.txt.gz', sep='\t')
 
-
-meta_data = pd.read_csv('Hackathon2024.Meta.txt.gz', sep='\t')
-
+print(rna_data, atac_data, train_pairs, test_pairs, meta_data)
 
 """
 Step 2:
